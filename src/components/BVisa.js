b085ea46a7d04d5169376f89a46fcc475439fee8
Rafael Freitas <rafael.frb@hotmail.com>
2023-01-14 10:05:00 +0000

    Working on BVisa component

--- src/components/BVisa.js
+++ src/components/BVisa.js
@@ -15,17 +15,27 @@ export default function BVisa () {
                     </div>
                     <div className='text-container'>
                     <p>O visto americano de turismo (B2) permite que um indivíduo entre temporariamente nos Estados Unidos
-                         por motivos de lazer/férias ou para visitar amigos ou familiares.
-                         A permanência máxima nos EUA é, normalmente, de 6 meses podendo ser extendida em certas circunstâncias.
+                         por motivos de lazer/férias ou para visitar amigos ou familiares.<br />
+                         A permanência máxima nos EUA é, normalmente, de 6 meses podendo ser extendida em certas circunstâncias.<br />
                         Para aplicar para um visto de turismo é necessário o preenchimento de um formulário, possuir passaporte
                         válido por pelo menos seis meses além da data em que pretende estar nos EUA e realizar o pagamento de uma
-                        taxa não-reembolsável.
-                        Pode ser necessário demonstrar comprovação de capacidade financeira, passagem de volta e um objetivo claro para a viagem.
-                        É importante saber que um visto de turismo não permite que seu portador trabalhe ou estude nos EUA.
+                        taxa não-reembolsável.<br />
+                        Pode ser necessário demonstrar comprovação de capacidade financeira, passagem de volta e um objetivo claro para a viagem.<br />
+                        É importante saber que um visto de turismo não permite que seu portador trabalhe ou estude nos EUA.<br />
                         Adicionalmente, é importante checar o site da Embaixada americana mais próxima para conhecer as regulações atualizadas
                         com relação à obtenção do seu visto.</p>
                     </div>
                 </div>
+                <br />
+                <h2>Passos para obter o visto de turismo (B2)</h2>
+                <ol className='to-do-list'>
+                    <li>1 - Possuir passaporte</li>
+                    <li>2 - Preencher formulário DS-160</li>
+                    <li>3 - Pagamento da taxa de solicitação de visto US$160</li>
+                    <li>4 - Realizar agendamento</li>
+                    <li>5 - Comparecer ao CASV para coleta de impressões digitais e fotografia</li>
+                    <li>6 - Comparecer ao consulado para entrevista</li>
+                </ol>
             </div>
             </div>
         </div>

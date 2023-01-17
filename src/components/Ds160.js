415b38cebb24e24784be2c7c8b49ae5238d02626
Rafael Freitas <rafael.frb@hotmail.com>
2023-01-17 12:20:56 +0000

    Working on DS-160 first page

--- src/components/Ds160.js
+++ src/components/Ds160.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import DS160_1 from './images/DS-160/DS-160_1.png';
+import './Ds160.css';
 
 export default function DS160 () {
     return (
-        <div>
-            <img src={DS160_1} />
+        <div className='bg-image-Ds160'>
+            <div className='bg-image'>
+                <img src={DS160_1} />
+            </div>
         </div>
     )
 }
